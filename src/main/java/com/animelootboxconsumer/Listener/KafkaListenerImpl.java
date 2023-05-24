e7e8b1ffea60cbab9df541fd3a60f6c12738787f
Lucas Peixoto <lucas.peixoto@venhapranuvem.com.br>
2023-05-23 21:55:50 -0300

    init repo

--- src/main/java/com/animelootboxconsumer/Listener/KafkaListenerImpl.java
+++ src/main/java/com/animelootboxconsumer/Listener/KafkaListenerImpl.java
@@ -3,7 +3,6 @@ package com.animelootboxconsumer.Listener;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.apache.kafka.common.protocol.Message;
 import org.springframework.stereotype.Component;
 
 @Slf4j
